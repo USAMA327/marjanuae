2528fc8e7210e83d0326d47fac0662ec87955a92
Muhammad Usama <48561276+USAMA327@users.noreply.github.com>
2025-04-07 17:35:06 +0500

    Add Widget Text

--- src/app/google-reviews/page.tsx
+++ src/app/google-reviews/page.tsx
@@ -1,24 +1,10 @@
 'use client'
-import React, { useEffect } from 'react'
-import { ElfsightWidget } from 'react-elfsight-widget'
-
+import React from 'react'
+import {ElfsightWidget} from "react-elfsight-widget"
 function GoogleReviews() {
-  useEffect(() => {
-    const observer = new MutationObserver(() => {
-      const badge = document.querySelector('a[href*="elfsight.com/google-reviews-widget"]');
-      if (badge) {
-        badge.remove(); // or badge.style.display = "none";
-      }
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <div>
-      <ElfsightWidget widgetId='8249fbce-39f8-4ae7-9cab-fa0f6e4c0b2a' />
+      <div className=''>
+          <ElfsightWidget widgetId='8249fbce-39f8-4ae7-9cab-fa0f6e4c0b2a'/>
     </div>
   )
 }
