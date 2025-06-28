c84a25cbcc6e20daf3447bfacb56e445b95496a0
Muhammad Usama <157679314+umermunir786@users.noreply.github.com>
2025-06-28 22:58:18 +0500

    Add /home url

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
   images: {
     remotePatterns: [
       {
-    protocol: 'https',
-    hostname: 'i.ibb.co',
-    pathname: '**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'lh3.googleusercontent.com',
-      pathname: '**',
-    }]
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true, // HTTP 308 (permanent redirect)
+      },
+    ];
   },
 };
 
